db7401c05e302ad3e46f4da1d8da1e5d560b5990
Krati97 <kabara.krati@gmail.com>
2025-07-24 13:45:45 -0700

    Missing Number (286)

---
+++ dataStructureAlgorithum/missingNumber.js
@@ -0,0 +1,27 @@
+// LC Problem 268:
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let largestConsecutive = nums.length;
+    console.log("Largest Consecutive: ", largestConsecutive);
+    let numsSum = 0;
+    let ConsecutiveSum = (largestConsecutive * (largestConsecutive + 1)) / 2
+    console.log("Consecutive Sum: ", ConsecutiveSum);
+
+    for(let i = 0; i < nums.length; i++){
+        numsSum = numsSum + nums[i];
+        }
+    console.log("Nums Sum: ", numsSum);
+    console.log("Missing Number: ", ConsecutiveSum - numsSum); 
+    return ConsecutiveSum - numsSum;
+    
+};
+
+console.log(missingNumber([3,0,1])); // 2
+console.log(missingNumber([0,1])); // 2
+console.log(missingNumber([9,6,4,2,3,5,7,0,1])); // 8
+
