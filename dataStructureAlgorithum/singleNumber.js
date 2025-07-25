bdf0cc7bcd72c8ff86e4bf727c54c3aad6320ff7
Krati97 <kabara.krati@gmail.com>
2025-07-25 16:15:39 -0700

    Single Number part 2 (136)

--- dataStructureAlgorithum/singleNumber.js
+++ dataStructureAlgorithum/singleNumber.js
@@ -29,3 +29,17 @@ var singleNumber = function(nums) {
 console.log(singleNumber([2,2,1])); // 1
 console.log(singleNumber([4,1,2,1,2])); // 4
 console.log(singleNumber([1])); // 1
+
+
+// Approach 2: Math
+// Use the property of XOR
+
+var singleNumber2 = function(nums) {
+let xor = 0;
+for( let i = 0; i < nums.length; i++){
+    xor = xor ^ nums[i];
+ }
+ return xor;
+};
+
+console.log("Solution using Xor: ",singleNumber2([5,1,3,4,5,1,3])); // Solution using Xor:  4
