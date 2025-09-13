abdd720318e8c46f27ea09590b73c74b5e02c40d
Krati97 <kabara.krati@gmail.com>
2025-09-12 19:59:25 -0700

    Bubble sort better approach

--- dataStructureAlgorithum/bubbleSort.js
+++ dataStructureAlgorithum/bubbleSort.js
@@ -18,3 +18,24 @@ function bubbleSort(arr){
 }
 
 console.log(bubbleSort(arr)); // output: [0, 1, 4, 5, 9]
+
+// if in any iteration, no swap is done, then array is already sorted.
+
+function bubbleSort2(arr){
+    let n = arr.length;
+    for(let i = 0; i < n - 1; i++){
+
+        let isSwapped = false; // to check if any swap is done in the iteration
+
+        for (let j = 0; j < n - 1 - i; j++){
+            if( arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                isSwapped = true; // if swap is done, then array is not sorted
+            }
+        }
+        if (!isSwapped) break; // if no swap is done, then array is already sorted
+    }
+    return arr;
+}
